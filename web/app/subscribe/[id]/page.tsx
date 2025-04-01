4e074bd0f823d2aa30d9f47e6e4ea1e3dc0a6fc0
YAlnasiri <alnasiriyusuf@gmail.com>
2025-04-01 04:33:03 +0300

    aa

--- web/app/subscribe/[id]/page.tsx
+++ web/app/subscribe/[id]/page.tsx
@@ -11,8 +11,7 @@ interface Answer {
 }
 
 export default function QuestionsPage({params}:{params: Promise<{id: string}>}) {
-  const resolvedParams = use(params);
-  const id = resolvedParams.id;
+  const id = use(params).id;
   const [gender, setGender] = useState<'male' | 'female' | null>(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState<Answer[]>([]);
